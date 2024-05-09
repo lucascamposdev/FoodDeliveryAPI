9abfd053f9ba577145f725e391fe535bd5574242
Lucas <prestoinker@gmail.com>
2024-05-09 16:03:13 -0300

    Initializing project generated by Spring Initialzr

---
+++ src/test/java/presto/com/FoodDeliveryAPI/FoodDeliveryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package presto.com.FoodDeliveryAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodDeliveryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
