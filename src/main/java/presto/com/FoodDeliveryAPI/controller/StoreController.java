970aed7914ef6c843b7cf4cc0016e6d7fada1d3b
Lucas <prestoinker@gmail.com>
2024-05-13 17:07:38 -0300

    creating findAll controller method and service

--- src/main/java/presto/com/FoodDeliveryAPI/controller/StoreController.java
+++ src/main/java/presto/com/FoodDeliveryAPI/controller/StoreController.java
@@ -1,22 +1,22 @@
 package presto.com.FoodDeliveryAPI.controller;
 
 import jakarta.validation.Valid;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import presto.com.FoodDeliveryAPI.dtos.store.StoreMapper;
+import presto.com.FoodDeliveryAPI.dtos.store.StoreMinimalResponseDto;
 import presto.com.FoodDeliveryAPI.dtos.store.StoreRequestDto;
 import presto.com.FoodDeliveryAPI.dtos.store.StoreResponseDto;
 import presto.com.FoodDeliveryAPI.service.StoreService;
 
 @RestController
-@RequestMapping("/stores")
+@RequestMapping("/api/v1/stores")
 public class StoreController {
 
-    private StoreService storeService;
+    private final StoreService storeService;
 
     public StoreController(StoreService storeService) {
         this.storeService = storeService;
@@ -24,7 +24,15 @@ public class StoreController {
 
     @PostMapping
     public ResponseEntity<StoreResponseDto> register(@RequestBody @Valid StoreRequestDto dto){
-        var createdStore = StoreMapper.returnStoreResponseFromStoreEntity(storeService.register(dto));
-        return ResponseEntity.status(HttpStatus.CREATED).body(createdStore);
+        var createdStore = storeService.register(dto);
+        var createdStoreResponse = StoreMapper.toResponse(createdStore);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdStoreResponse);
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<StoreMinimalResponseDto>> findAll(Pageable page){
+        var list = storeService.findAll(page);
+        var listResponse = list.map(StoreMapper::toMinimalResponse);
+        return ResponseEntity.status(HttpStatus.CREATED).body(listResponse);
     }
 }
--- src/main/java/presto/com/FoodDeliveryAPI/service/StoreService.java
+++ src/main/java/presto/com/FoodDeliveryAPI/service/StoreService.java
@@ -1,5 +1,7 @@
 package presto.com.FoodDeliveryAPI.service;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import presto.com.FoodDeliveryAPI.dtos.store.StoreMapper;
 import presto.com.FoodDeliveryAPI.dtos.store.StoreRequestDto;
@@ -9,13 +11,17 @@ import presto.com.FoodDeliveryAPI.repository.StoreRepository;
 @Service
 public class StoreService {
 
-    private StoreRepository storeRepository;
+    private final StoreRepository storeRepository;
 
     public StoreService(StoreRepository storeRepository) {
         this.storeRepository = storeRepository;
     }
 
     public Store register(StoreRequestDto dto){
-        return storeRepository.save(StoreMapper.returnStoreFromStoreRequestDto(dto));
+        return storeRepository.save(StoreMapper.toEntity(dto));
+    }
+
+    public Page<Store> findAll(Pageable page){
+        return storeRepository.findAll(page);
     }
 }
