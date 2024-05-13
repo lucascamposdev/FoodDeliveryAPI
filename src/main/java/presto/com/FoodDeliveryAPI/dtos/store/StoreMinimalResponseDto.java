267ecb9a726e7786ea82042ab65470e7dd60ab5f
Lucas <prestoinker@gmail.com>
2024-05-13 17:06:03 -0300

    adds StoreMinimalResponseDto

--- src/main/java/presto/com/FoodDeliveryAPI/dtos/store/StoreMinimalResponseDto.java
+++ src/main/java/presto/com/FoodDeliveryAPI/dtos/store/StoreMinimalResponseDto.java
@@ -1,2 +1,16 @@
-package presto.com.FoodDeliveryAPI.dtos.store;public class StoreMinimalResponseDto {
+package presto.com.FoodDeliveryAPI.dtos.store;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import presto.com.FoodDeliveryAPI.entity.Address;
+import presto.com.FoodDeliveryAPI.entity.OpeningHours;
+
+import java.util.List;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StoreMinimalResponseDto {
+    private String name;
 }
--- src/main/java/presto/com/FoodDeliveryAPI/dtos/store/StoreResponseDto.java
+++ src/main/java/presto/com/FoodDeliveryAPI/dtos/store/StoreResponseDto.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Embedded;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import presto.com.FoodDeliveryAPI.entity.Address;
 import presto.com.FoodDeliveryAPI.entity.OpeningHours;
 
@@ -12,6 +13,7 @@ import java.util.List;
 
 @Getter
 @AllArgsConstructor
+@NoArgsConstructor
 public class StoreResponseDto {
 
     private String name;
