51f5b28521b5ebc5c868c81c229728ab67954796
Lucas <prestoinker@gmail.com>
2024-05-14 13:35:58 -0300

    adds Authorization Service file

---
+++ src/main/java/presto/com/FoodDeliveryAPI/service/AuthorizationService.java
@@ -0,0 +1,18 @@
+package presto.com.FoodDeliveryAPI.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import presto.com.FoodDeliveryAPI.repository.UserRepository;
+
+@Service
+public class AuthorizationService implements UserDetailsService {
+    @Autowired
+    UserRepository repository;
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByEmail(username);
+    }
+}
