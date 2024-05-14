907453a4edfa959ce928ecc04a33a47a70805a4d
Lucas <prestoinker@gmail.com>
2024-05-14 13:44:41 -0300

    adds Token Service file

--- src/main/java/presto/com/FoodDeliveryAPI/infra/security/TokenService.java
+++ src/main/java/presto/com/FoodDeliveryAPI/infra/security/TokenService.java
@@ -1,2 +1,52 @@
-package presto.com.FoodDeliveryAPI.infra.security;public class TokenService {
+package presto.com.FoodDeliveryAPI.infra.security;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import presto.com.FoodDeliveryAPI.entity.User;
+
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+
+@Service
+public class TokenService {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(User user){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.create()
+                    .withIssuer("foodDelivery-api")
+                    .withSubject(user.getUsername())
+                    .withExpiresAt(generateExpirationDate())
+                    .sign(algorithm);
+        }catch (JWTCreationException ex){
+            throw new RuntimeException("Erro ao gerar token", ex);
+        }
+    }
+
+    public String validateToken(String token){
+        try{
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+
+            return JWT.require(algorithm)
+                    .withIssuer("foodDelivery-api")
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        }catch (JWTVerificationException ex){
+            System.out.println("Token não válido");
+            return "";
+        }
+    }
+
+    private Instant generateExpirationDate (){
+        return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-03:00"));
+    }
 }
